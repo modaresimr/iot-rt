cc1b83f7738e72f7295582c77062aaa2670f83de
alim1369 <alim1369@gmail.com>
2019-06-25 09:27:39 +0200

    no pr test

--- iot_defaults.php
+++ iot_defaults.php
@@ -162,11 +162,12 @@ function sendtestEmail(){
     $email="alim1369@gmail.com";
     $password_reset_ket = get_password_reset_key(wp_get_current_user());
 	$rp_link = '<a href="' . network_site_url("wp-login.php?action=rp&key=$password_reset_ket&login=" . rawurlencode($email), 'login') . '">' . network_site_url("wp-login.php?action=rp&key=$password_reset_ket&login=" . rawurlencode($email), 'login') . '</a>';
-					wp_mail($email, 'IoT-rt.ml Registeration Success', 'To set your password Please click on ' . $rp_link,array('Content-Type: text/html; charset=UTF-8'));
+	echo wp_mail($email, 'IoT-rt.ml Registeration Success', 'To set your password Please click on ' . $rp_link,array('Content-Type: text/html; charset=UTF-8'));
     
 
 }
 add_action('wp_ajax_iot_testemail', 'sendtestEmail');
+add_action('wp_ajax_nopriv_iot_testemail', 'sendtestEmail');
 function addDefaultUni()
 {
 
